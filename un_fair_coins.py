56b4d9dd67b43b9c9007997c1463135e60ee658b
victoria <victoria.hodder@signavio.com>
2021-10-19 09:14:12 +0200

    add new lines

--- un_fair_coins.py
+++ un_fair_coins.py
@@ -2,26 +2,26 @@
 
 import random
 
-def coin_flip():
-    """Randomly return 'heads' or 'tails'."""
-    if random.randint(0, 1) == 0:
-        return "heads"
-    else:
-        return "tails"
-
-
-
-heads_tally = 0
-tails_tally = 0
-
-for trial in range(10_000):
-    if coin_flip() == "heads":
-        heads_tally = heads_tally + 1
-    else:
-        tails_tally = tails_tally + 1
-
-ratio = heads_tally / tails_tally
-print(f"The ratio of heads to tails is {round(ratio, 2)}")
+# def coin_flip():
+#     """Randomly return 'heads' or 'tails'."""
+#     if random.randint(0, 1) == 0:
+#         return "heads"
+#     else:
+#         return "tails"
+#
+#
+#
+# heads_tally = 0
+# tails_tally = 0
+#
+# for trial in range(10_000):
+#     if coin_flip() == "heads":
+#         heads_tally = heads_tally + 1
+#     else:
+#         tails_tally = tails_tally + 1
+#
+# ratio = heads_tally / tails_tally
+# print(f"The ratio of heads to tails is {round(ratio, 2)}")
 
 """ Unfair coins """
 
@@ -30,6 +30,8 @@ def unfair_coin_flip(probability_of_tails):
         return "tails"
     else:
         return "heads"
+
+# print(unfair_coin_flip(0.4))
 
 
 heads_tally = 0
@@ -41,5 +43,8 @@ for trial in range(10_000):
     else:
         tails_tally = tails_tally + 1
 
+print(f"Heads tally: {heads_tally}")
+print(f"Tails tally: {tails_tally}")
+
 ratio = heads_tally / tails_tally
-# print(f"The ratio of heads to tails is {round(ratio, 2)}")
+print(f"The ratio of heads to tails is {round(ratio, 2)}")
