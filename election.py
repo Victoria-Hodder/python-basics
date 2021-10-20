27a3398f6badf4b8e238bbc44c834dc2e98b3bad
victoria <victoria.hodder@signavio.com>
2021-10-20 09:16:30 +0200

    remove comments

--- election.py
+++ election.py
@@ -1,5 +1,3 @@
-# create a function for probability A wins
-
 import random
 
 def election(probability_A_wins):
@@ -8,11 +6,7 @@ def election(probability_A_wins):
     else:
         return "B wins"
 
-# print(election(0.87))
-# print(election(0.65))
-# print(election(0.17))
-
-no_of_elections = 10_000
+nr_of_elections = 10_000
 region1 = 0.87
 region2 = 0.65
 region3 = 0.17
@@ -20,7 +14,7 @@ region3 = 0.17
 candidateA_count = 0
 candidateB_count = 0
 
-for i in range(no_of_elections):
+for i in range(nr_of_elections):
     if election(region1) == "A wins":
         candidateA_count += 1
     else:
