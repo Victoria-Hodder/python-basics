7d1ffc2aea79f1af9cee204ca18fd603efcbc513
victoria <victoria.hodder@signavio.com>
2021-10-20 08:52:10 +0200

    calculate percentage of candidate A winning for one region

--- election.py
+++ election.py
@@ -2,12 +2,33 @@
 
 import random
 
-def region(probability_A_wins):
+def election(probability_A_wins):
     if random.random() < probability_A_wins:
         return "A wins"
     else:
         return "B wins"
 
-print(region(0.87))
-print(region(0.65))
-print(region(0.17))
+# print(election(0.87))
+# print(election(0.65))
+# print(election(0.17))
+
+no_of_elections = 10_000
+region1 = 0.87
+region2 = 0.65
+region3 = 0.17
+
+candidateA_count = 0
+candidateB_count = 0
+
+for i in range(no_of_elections):
+    if election(region1) == "A wins":
+        candidateA_count += 1
+    else:
+        candidateB_count += 1
+
+print(f"candidateA_count: {candidateA_count}")
+print(f"candidateB_count: {candidateB_count}")
+
+percentage = (candidateA_count / 10_000) * 100
+
+print(f"Percentage A wins: {round(percentage, 2)}")
