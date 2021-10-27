94c1bacc954937b54d0c925266e98ba7912e213a
victoria <victoria.hodder@signavio.com>
2021-10-27 08:58:02 +0200

    add final step in which an election is only won if a candidate wins in 2 or more of the regions

--- election.py
+++ election.py
@@ -10,27 +10,49 @@ def simulate_election(probability_A_wins):
         return "B"
 
 
-def count_candidate_wins():
+def count_candidate_wins_by_region():
     """
     Counts the number of times each candidate wins
-    in each of the three regions.
+    in each of the three regions in one election run.
+
+    For simplicity this function only returns a list
+    of the count candidate wins in each region
     """
-    nr_of_elections = 10_000
-    # List probability that A wins in each region
+    # List containing probability that A wins in each region
     regions = [0.87, 0.65, 0.17]
     candidateA_count = [0, 0, 0]
     candidateB_count = [0, 0, 0]
 
-    for election in range(nr_of_elections):
-        for count in range(0, 3):
-            if simulate_election(regions[count]) == "A":
-                candidateA_count[count] += 1
-            else:
-                candidateB_count[count] += 1
+    for count in range(0, 3):
+        if simulate_election(regions[count]) == "A":
+            candidateA_count[count] += 1
+        else:
+            candidateB_count[count] += 1
 
-    print(f"candidateA_count: {candidateA_count}")
-    print(f"candidateB_count: {candidateB_count}")
     return sum(candidateA_count)
+
+def count_candidate_wins():
+    """
+    Count the number of times each candidate wins in a
+    run of 10,000 elections.
+
+    The candidate wins the election if they in at least
+    two of the three regions
+    """
+    nr_of_elections = 10_000
+    count_a_wins = 0
+    count_b_wins = 0
+
+    for election in range(nr_of_elections):
+        if count_candidate_wins_by_region() >= 2:
+            count_a_wins += 1
+        else:
+            count_b_wins += 1
+
+    # print(f"candidateA_election_wins: {count_a_wins}")
+    # print(f"candidateB_election_wins: {count_b_wins}")
+    return count_a_wins
+
 
 def calculate_percentage():
     """
@@ -38,12 +60,7 @@ def calculate_percentage():
     within the given nr of elections (10_000).
     """
     total_a_wins = count_candidate_wins()
-    percentage_a_wins = (10_000 / total_a_wins) * 100
+    percentage_a_wins = (total_a_wins / 10_000) * 100
     return f"Candidate A wins {round(percentage_a_wins, 2)}% of the time"
 
 print(calculate_percentage())
-
-"""
-Left to do: "assume that a candidate wins the election if
-they win in at least two of the three regions."
-"""
