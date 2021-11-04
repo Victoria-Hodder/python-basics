5f2a4fc9929e721c409f3f1f27a00c8e3f046960
victoria-hodder <vrhodder@posteo.net>
2021-11-04 15:48:25 +0100

    play with case statements

--- SQL/report.sql
+++ SQL/report.sql
@@ -1,8 +1,18 @@
 
 -- Joining Students and Grades tables using WHERE
-select s.name, s.marks, g.grade
+select s.name, g.grade, s.marks
 from students s, grades g
 where s.marks between g.min_mark and g.max_mark
-order by g.grade desc;
+order by g.grade desc, s.name;
 
 -- Idea for join with JOIN:  'join abc on x >=  min and x  <= max'
+
+-- Playing with CASE
+select s.name, g.grade, s.marks,
+case
+    when g.grade < 8 then 'The grade is less than 8'
+    else 'The grade is equal to or more than 8'
+end
+from students s, grades g
+where s.marks between g.min_mark and g.max_mark
+order by g.grade desc, s.name;
