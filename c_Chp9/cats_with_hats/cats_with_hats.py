b73752b55749207bbee8e5373ece588a5a2f8637
victoria-hodder <vrhodder@posteo.net>
2021-12-02 09:21:35 +0100

    play with cat dictionary and booleans

--- c_Chp9/cats_with_hats/cats_with_hats.py
+++ c_Chp9/cats_with_hats/cats_with_hats.py
@@ -1,8 +1,26 @@
 
 def generate_cats():
     cats = []
-    for cat in range(1, 101):
-        cats.append(f'#{cat}')
+    for cat in range(1, 11):
+        cats.append(cat)
     return cats
 
 print(generate_cats())
+
+def generate_cats_dict():
+    cats = {}
+    for cat in range(1, 11):
+        cats[cat] = True
+    return cats
+
+print(generate_cats_dict())
+
+def convert_even_keys_to_false():
+    cats = generate_cats_dict()
+
+    for key, value in cats.items():
+        if key % 2 == 0:
+            cats[key] = False
+    return cats
+
+print(convert_even_keys_to_false())
