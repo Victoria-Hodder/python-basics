93f696d6a5a32a10bf49c27e9b461ebedc42775b
victoria-hodder <vrhodder@posteo.net>
2021-12-07 08:10:27 +0100

    add documentation

--- c_Chp9/cats_with_hats/cats_with_hats.py
+++ c_Chp9/cats_with_hats/cats_with_hats.py
@@ -1,5 +1,9 @@
 
 def put_hats_on_cats():
+    """
+    Place a hat on every cat by assigning each numbered key
+    with a True value
+    """
     cats = {}
     for cat in range(1, 101):
         cats[cat] = True
@@ -8,6 +12,17 @@ def put_hats_on_cats():
 # print(put_hats_on_cats())
 
 def remove_or_put_hat_on_cat():
+    """
+    Make 100 rounds around the cats starting with the second round
+    (the first round is made in put_hats_on_cats())
+
+    In the second round, stop only at every second cat (#2, #4, #6 etc)
+
+    The third round, stop only at every third cat (#3, #6, #9 etc)
+
+    Continue this process until one hundred rounds are made.
+    On the last round, stop only at cat #100.
+    """
     cats = put_hats_on_cats()
 
     for round in range(2,101):
@@ -16,20 +31,19 @@ def remove_or_put_hat_on_cat():
                 cats[key] = False
             elif key % round == 0 and value == False:
                 cats[key] = True
-#         print(f"Round {round}: {cats}\n")
     return cats
 
-# print(remove_or_put_hat_on_cat())
-
 def output_cats_with_hats():
+    """
+    Create a list which returns the numbered keys which have a true value
+    (i.e. all the cats which have hats)
+    """
     cats = remove_or_put_hat_on_cat().items()
     cats_with_hats = []
 
     for key, value in cats:
         if value == True:
-            # append key to list
             cats_with_hats.append(key)
-    # return list of cats with hats
     return cats_with_hats
 
 print(output_cats_with_hats())
