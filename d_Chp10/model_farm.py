fed3e11628022032f5b4c82d7ab3344d68a6eb0a
victoria-hodder <vrhodder@posteo.net>
2022-01-07 09:28:08 +0100

    extending child classes

--- d_Chp10/model_farm.py
+++ d_Chp10/model_farm.py
@@ -20,36 +20,55 @@ class Chicken(Animal):
     def by_product(self):
         return f"{self.name} lays eggs"
 
+    def fun_fact(self):
+        return f"Did you know? Chickens are the closest living relative to the T-Rex"
+
 class Cow(Animal):
-    def __init__(self, animal, name):
+    def __init__(self, animal, name, hobby):
         super().__init__(animal, name)
+        self.hobby = hobby
 
     def by_product(self):
         return f"{self.name} produces milk"
 
-class Donkey(Animal):
-    def __init__(self, animal, name):
-        super().__init__(animal, name)
+    def free_time(self):
+        return f"In her free time, {self.name} likes to {self.hobby}"
 
-    def task(self, burden="small children"):
-        return f"{self.name} the {self.animal} carries {burden}"
+    def fun_fact(self):
+        return f"Did you know? A cow will chew for up to eight hours a day"
+
+class Donkey(Animal):
+    def __init__(self, animal, name, fave_food):
+        super().__init__(animal, name)
+        self.fave_food = fave_food
 
     def speak(self, sound="hee-haw"):
         return super().speak(sound)
 
+    def task(self, burden="small children"):
+        return f"{self.name} the donkey carries {burden}"
+
+    def eat(self):
+        return f"{self.name} the donkey likes to eat {self.fave_food}"
+
+    def fun_fact(self):
+        return f"Did you know? The smallest donkey recorded is only 19 inches tall"
+
+
 
 chicken = Chicken("chicken", "Delilah", "green eyes and feathers")
-cow = Cow("cow", "Daisy")
-donkey = Donkey("donkey", "Elif")
+cow = Cow("cow", "Daisy", "go bungee-jumping")
+donkey = Donkey("donkey", "Elif", "cheese burgers")
 
 print(chicken)
 print(chicken.description())
 print(chicken.by_product(), "\n")
 
 print(cow)
-print(cow.speak("mooooo"))
-print(cow.by_product(), "\n")
+print(cow.by_product())
+print(cow.free_time())
+print(cow.fun_fact(), "\n")
 
 print(donkey)
-print(donkey.task())
+print(donkey.eat())
 print(donkey.speak())
