869258e1871416cd01bd7ab9d16f16746d1ed005
victoria-hodder <vrhodder@posteo.net>
2022-01-24 09:10:39 +0100

    working with modules

---
+++ e_Chp11/main.py
@@ -0,0 +1,9 @@
+import adder
+import greeter
+
+value = adder.add(2,2)
+double_value = adder.double(value)
+print(double_value)
+
+greet_real_python = greeter.greet("Real Python")
+print(greet_real_python)
