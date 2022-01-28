370dfc31d82cd014558babd5233be2c16833ade8
victoria-hodder <vrhodder@posteo.net>
2022-01-28 09:31:11 +0100

    add list loop

--- e_Chp11/package_exercises/main.py
+++ e_Chp11/package_exercises/main.py
@@ -1,6 +1,8 @@
 from helpers.math import area
 from helpers.string import shout
 
-length = 5
-width = 3
-print(shout(f"the area of a {length} by {width} rectangle is {area(length, width)}"))
+lengths = [5,6,7]
+widths = [3,2,9]
+
+for length, width in zip(lengths, widths):
+    print(shout(f"the area of a {length} by {width} rectangle is {area(length, width)}"))
