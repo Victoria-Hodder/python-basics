a558bf9ce42dfe56bb5b55eeb8f42fb6420c5e51
victoria-hodder <vrhodder@posteo.net>
2022-02-16 08:16:45 +0100

    Initial attempt at moving image files into one directory

---
+++ f_Chp12/challenge.py
@@ -0,0 +1,33 @@
+"""
+Create a new folder in the practice_files folder called images/ , and
+move all image ﬁles to that folder. When you’re done, the new folder
+should have four ﬁles in it.
+"""
+
+import pathlib
+
+cwd = pathlib.Path.cwd()
+img_dir = cwd / "practice_files" / "images"
+img_dir.mkdir(exists_ok=True)
+
+docs_dir = cwd / "practice_files" / "documents"
+
+image1_source = docs_dir / "image1.png"
+image1_destination = img_dir / "image1.png"
+if not image1_destination.exists():
+    image1_source.replace(image1_destination)
+
+image2_source = docs_dir / "more_files" / "image2.gif"
+image2_destination = img_dir / "image2.gif"
+if not image2_destination.exists():
+    image2_source.replace(image2_destination)
+
+image3_source = docs_dir / "files" / "additional files" / "image3.png"
+image3_destination = img_dir / "image3.png"
+if not image3_destination.exists():
+    image3_source.replace(image3_destination)
+
+image4_source = docs_dir / "more_files" / "even_more_files" / "image4.jpg"
+image4_destination = img_dir / "image4.png"
+if not image4_destination.exists():
+    image4_source.replace(image4_destination)
