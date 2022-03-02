8015d3e4deadc73635f3242c16f46b153d0c20c5
victoria-hodder <vrhodder@posteo.net>
2022-03-02 19:59:14 +0100

    Collect high scores using dictionary and list of dictionaries

--- f_Chp12/challenge/scores.py
+++ f_Chp12/challenge/scores.py
@@ -20,24 +20,35 @@ with scores.open(mode="r", encoding="utf-8", newline="") as file:
 
 # print(scores_list)
 
-# Do something to find max scores here?
 user_scores = {}
 
 for item in scores_list:
     if item['name'] not in user_scores:
         user_scores[item['name']] = [] # emtpy list to add one by one the scores
+    user_scores[item['name']].append(item['score'])
+    # print(user_scores)
 
-print(user_scores)
+# print(user_scores)
 
-# here add the score in the list of that name
-# you could also keeping only the max score instead of making a scores list and then doing max to them
+# high_scores as a dict: {"Victoria": 45, "Meili": 56}
+# high_scores = {}
+# for name in user_scores:
+#     high_scores[name] = max(user_scores[name])
+
+# high_scores as a list of dicts:
+# [{"name": "Victoria", "high_score": 142}, {"name": "Meili", "high_score": 62}]
+high_scores = []
+for name, scores in user_scores.items():
+    high_scores.append({"name": name, "high_score": max(scores)})
+
+print(high_scores)
 
 # renaming 'score' key to 'high_score'
-for item in scores_list:
-    item['high_score'] = item['score']
-    del item['score']
+# for item in high_scores:
+#     item['high_score'] = item['score']
+#     del item['score']
 
-with high_scores.open(mode='w', encoding='utf-8', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames = ["name", "high_score"])
-    writer.writeheader()
-    writer.writerows(scores_list)
+# with high_scores.open(mode='w', encoding='utf-8', newline='') as file:
+#     writer = csv.DictWriter(file, fieldnames = ["name", "high_score"])
+#     writer.writeheader()
+#     writer.writerows(scores_list)
