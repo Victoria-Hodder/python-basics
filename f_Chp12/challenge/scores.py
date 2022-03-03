b41ef75baede0ef959fcf3a43289fa9795e43be7
victoria-hodder <vrhodder@posteo.net>
2022-03-03 08:29:22 +0100

    Write to final csv and tidy

--- f_Chp12/challenge/scores.py
+++ f_Chp12/challenge/scores.py
@@ -1,10 +1,8 @@
 import csv
 from pathlib import Path
 
-high_scores = Path.home() / "Documents" / "high_scores.csv"
+destination = Path.home() / "Documents" / "high_scores.csv"
 scores = Path.home() / "workspace" / "python-basics" / "f_Chp12" / "challenge" / "scores.csv"
-
-scores_dict = {}
 
 # Make score value an integer
 def process_row(row):
@@ -15,22 +13,17 @@ scores_list = []
 with scores.open(mode="r", encoding="utf-8", newline="") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        # print(process_row(row))
         scores_list.append(process_row(row))
 
-# print(scores_list)
-
+# Collecting player scores
 user_scores = {}
-
 for item in scores_list:
     if item['name'] not in user_scores:
         user_scores[item['name']] = [] # emtpy list to add one by one the scores
     user_scores[item['name']].append(item['score'])
-    # print(user_scores)
 
-# print(user_scores)
-
-# high_scores as a dict: {"Victoria": 45, "Meili": 56}
+# high_scores as a dict:
+# {"Victoria": 45, "Meili": 56}
 # high_scores = {}
 # for name in user_scores:
 #     high_scores[name] = max(user_scores[name])
@@ -41,14 +34,7 @@ high_scores = []
 for name, scores in user_scores.items():
     high_scores.append({"name": name, "high_score": max(scores)})
 
-print(high_scores)
-
-# renaming 'score' key to 'high_score'
-# for item in high_scores:
-#     item['high_score'] = item['score']
-#     del item['score']
-
-# with high_scores.open(mode='w', encoding='utf-8', newline='') as file:
-#     writer = csv.DictWriter(file, fieldnames = ["name", "high_score"])
-#     writer.writeheader()
-#     writer.writerows(scores_list)
+with destination.open(mode='w', encoding='utf-8', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames = ["name", "high_score"])
+    writer.writeheader()
+    writer.writerows(high_scores)
